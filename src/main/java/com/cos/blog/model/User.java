dae80247b47d48a5d6f1c1f5a21b2ed84993d79d
윤호빈 <ad4733@naver.com>
2021-04-28 22:39:09 +0900

    테이블 생성

---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,44 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity //User 클래스가 MySQL에 테이블이 생성이 된다.
+public class User {
+	
+	@Id //Primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연결된 DB의 넘버링 전략을 따라간다. (MySQL = auto_increment)
+	private int id; //시퀀스, auto_increment
+	
+	@Column(nullable = false, length = 30)
+	private String username;
+	
+	@Column(nullable = false, length = 100)
+	private String password;
+	
+	@Column(nullable = false, length = 50)
+	private String email;
+	
+	@ColumnDefault("'user'")
+	private String role; //Enum을 쓰는게 좋다.
+	
+	@CreationTimestamp // 시간이 자동으로 입력이 된다.
+	private Timestamp creatDate;
+}
