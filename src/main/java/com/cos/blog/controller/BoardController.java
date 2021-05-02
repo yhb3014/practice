a8c802c674a5a1934b58f55d642ce1c39597166b
윤호빈 <ad4733@naver.com>
2021-05-02 18:32:49 +0900

    글삭제, 수정

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cos.blog.service.BoardService;
 
@@ -23,9 +24,21 @@ public class BoardController {
 		return "index";
 	}
 	
+	@GetMapping("/board/{id}")
+	public String findById(@PathVariable int id, Model model) {
+		model.addAttribute("board", boardService.글상세보기(id));
+		return "board/detail";
+	}
+	
 	//USER 권한이 필요
 	@GetMapping("/board/saveForm")
 	public String saveForm() {
 		return "board/saveForm";
+	}
+	
+	@GetMapping("/board/{id}/updateForm")
+	public String updateForm(@PathVariable int id, Model model){
+		model.addAttribute("board", boardService.글상세보기(id));
+		return "board/updateForm";
 	}
 }
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,7 +1,5 @@
 package com.cos.blog.service;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,18 +13,42 @@ import com.cos.blog.repisitory.BoardRepository;
 // 스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다
 @Service
 public class BoardService {
-	
+
 	@Autowired // 의존성 주입(DI)
 	private BoardRepository boardRepository;
-	
+
 	@Transactional
 	public void 글쓰기(Board board, User user) { // title, content
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
 	}
-	
-	public Page<Board> 글목록(Pageable pageable){
+
+	@Transactional(readOnly = true)
+	public Page<Board> 글목록(Pageable pageable) {
 		return boardRepository.findAll(pageable);
+	}
+
+	@Transactional(readOnly = true)
+	public Board 글상세보기(int id) {
+		return boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
+		});
+	}
+
+	@Transactional
+	public void 글삭제하기(int id) {
+		boardRepository.deleteById(id);
+	}
+
+	@Transactional
+	public void 글수정하기(int id, Board requestBoard) {
+		Board board = boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("글 찾기 실패 : 아이디를 찾을 수 없습니다.");
+		}); // 영속화 완료
+
+		board.setTitle(requestBoard.getTitle());
+		board.setContent(requestBoard.getContent());
+		// 해당 함수로 종료시 (Service가 종료될 때) 트랜잭션이 종료된다. 이때 더티체킹 - 자동 업데이트가 됨. DB쪽으로 flush
 	}
 }
